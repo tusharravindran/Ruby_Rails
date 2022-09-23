bbf4e91cb745fc58953222728b541bd29b04597d
tushar R <tusharrravindran@gmail.com>
2022-09-23 22:40:20 +0530

    tree

--- BinaryTravers.rb
+++ BinaryTravers.rb
@@ -1,1 +1,35 @@
-df
+tree = {
+  "value" => 1,
+  "left" => {
+    "value" => 2,
+    "left" => {
+      "value" => 4,
+      "left" => {
+        "value" => 4,
+      },
+    },
+    "right" => { "value" => 5 },
+  },
+  "right" => {
+    "value" => 3,
+    "left" => { "value" => 6 },
+    "right" => { "value" => 7 },
+  }
+}
+
+def transpose(hash)
+  hash.each do |key, value|
+    if value.is_a?(Hash)
+      transpose(value)
+      value['left'], value['right'] = value['right'], value['left']
+    end
+  end
+end
+
+transpose(tree)
+tree['left'], tree['right'] = tree['right'], tree['left']
+
+tree = tree.reject {
+  |key, value| 
+puts "#{key}: #{value} "
+}
